3b56bcfe0f0899f2e4f330877c5d5ff2217ed317
Nelson Dominguez <ekkolon@proton.me>
2024-11-13 17:31:25 +0100

    refactor: remove unused providers

--- apps/photolog/src/app/app.routes.ts
+++ apps/photolog/src/app/app.routes.ts
@@ -3,7 +3,6 @@ import { Route } from '@angular/router';
 import { provideEffects } from '@ngrx/effects';
 import { provideState } from '@ngrx/store';
 import * as fromImages from '@photolog/data-access-images';
-import { defaultResizeOptions, provideResizeOptions } from 'ngxtension/resize';
 import { queryParamsResolver } from './resolvers/photo-list.resolver';
 import { photoResolver } from './resolvers/photo.resolver';
 
@@ -19,17 +18,8 @@ export const appRoutes: Route[] = [
         (mod) => mod.PhotoListPage,
       ),
     providers: [
-      provideState(fromImages.IMAGES_FEATURE_KEY, fromImages.imagesReducer),
       provideState(fromImages.PAGES_FEATURE_KEY, fromImages.pagesReducer),
       provideEffects(fromImages.ImagesEffects, fromImages.PagesEffects),
-
-      provideResizeOptions({
-        ...defaultResizeOptions,
-        // emitInZone: true,
-        emitInitialResult: true,
-        scroll: false,
-        offsetSize: true,
-      }),
     ],
   },
   {
